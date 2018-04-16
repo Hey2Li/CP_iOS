ef1d5903d79e8e90986c8d8bb97b8a161769e9f6
litao <litao@cooplan.cn>
2018-04-16 11:49:58 +0800

    listen

---
+++ cooplaniOS/Home/ViewController/PaperDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaperDetailViewController.h
+//  cooplaniOS
+//
+//  Created by Lee on 2018/4/16.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaperDetailViewController : UIViewController
+
+@end
---
+++ cooplaniOS/Home/ViewController/PaperDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  PaperDetailViewController.m
+//  cooplaniOS
+//
+//  Created by Lee on 2018/4/16.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import "PaperDetailViewController.h"
+
+@interface PaperDetailViewController ()
+
+@end
+
+@implementation PaperDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
