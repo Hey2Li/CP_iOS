974ec22acd402f6f2f3dd05b36b3b1a2ecc011a1
litao <litao@cooplan.cn>
2018-05-11 17:21:00 +0800

    图片修改 添加收藏句子model

---
+++ cooplaniOS/cooplaniOS/Note/Model/collectionSentenceModel.h
@@ -0,0 +1,13 @@
+//
+//  collectionSentenceModel.h
+//  cooplaniOS
+//
+//  Created by Lee on 2018/5/11.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface collectionSentenceModel : NSObject
+
+@end
---
+++ cooplaniOS/cooplaniOS/Note/Model/collectionSentenceModel.m
@@ -0,0 +1,13 @@
+//
+//  collectionSentenceModel.m
+//  cooplaniOS
+//
+//  Created by Lee on 2018/5/11.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import "collectionSentenceModel.h"
+
+@implementation collectionSentenceModel
+
+@end
