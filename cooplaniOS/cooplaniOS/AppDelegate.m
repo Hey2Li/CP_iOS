c21a1e1fb6829a73e7c96e787bc9ad0585903198
litao <litao@cooplan.cn>
2018-05-14 13:48:53 +0800

    前后台判断

--- cooplaniOS/cooplaniOS/AppDelegate.m
+++ cooplaniOS/cooplaniOS/AppDelegate.m
@@ -116,12 +116,14 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     NSLog(@"进入后台");
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"listenBackground" object:nil];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     NSLog(@"回到前台");
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"listenForeground" object:nil];
 }
 
 
